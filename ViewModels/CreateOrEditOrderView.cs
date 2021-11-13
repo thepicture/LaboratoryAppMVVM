019850a8858c121ee7c960c2edfbe5613b243011
thepicture <85333076+thepicture@users.noreply.github.com>
2021-11-13 09:42:20 +0000

    Added tooltip and an enter event to it, many services adding in CreateOrEditOrderView

--- ViewModels/CreateOrEditOrderView.cs
+++ ViewModels/CreateOrEditOrderView.cs
@@ -3,9 +3,8 @@ using LaboratoryAppMVVM.Models.Entities;
 using LaboratoryAppMVVM.Stores;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LaboratoryAppMVVM.ViewModels
 {
@@ -13,10 +12,13 @@ namespace LaboratoryAppMVVM.ViewModels
     {
         private readonly ViewModelNavigationStore _navigationStore;
         private Order _order;
-        private List<AppliedService> _appliedServicesList;
+        private ObservableCollection<AppliedService> _appliedServicesList;
         private LaboratoryDatabaseEntities _context;
         private RelayCommand _addServiceToServicesViewCommand;
         private RelayCommand _navigateToLaboratoryAssistantViewModel;
+        private RelayCommand _enterTooltipCommand;
+        private string _tubeIdTooltipText = "Введите код пробирки...";
+        private string _tubeId;
 
         public CreateOrEditOrderView(ViewModelNavigationStore navigationStore, User user, Order order)
         {
@@ -43,13 +45,13 @@ namespace LaboratoryAppMVVM.ViewModels
             }
         }
 
-        public List<AppliedService> AppliedServicesList
+        public ObservableCollection<AppliedService> AppliedServicesList
         {
             get
             {
                 if (_appliedServicesList == null)
                 {
-                    _appliedServicesList = Order.AppliedService.ToList();
+                    _appliedServicesList = new ObservableCollection<AppliedService>(Order.AppliedService);
                 }
                 return _appliedServicesList;
             }
@@ -79,7 +81,8 @@ namespace LaboratoryAppMVVM.ViewModels
             {
                 if (_addServiceToServicesViewCommand == null)
                 {
-                    _addServiceToServicesViewCommand = new RelayCommand(param => AppliedServicesList.Add(new AppliedService()));
+                    _addServiceToServicesViewCommand =
+                        new RelayCommand(param => AppliedServicesList.Add(new AppliedService()));
                 }
                 return _addServiceToServicesViewCommand;
             }
@@ -96,6 +99,51 @@ namespace LaboratoryAppMVVM.ViewModels
                         new LaboratoryAssistantViewModel(_navigationStore, User));
                 }
                 return _navigateToLaboratoryAssistantViewModel;
+            }
+        }
+
+        public string TubeIdTooltipText
+        {
+            get
+            {
+                if (Context.Order.Any())
+                {
+                    _tubeIdTooltipText = Convert.ToString(Context.Order.ToList().Last().Id + 1);
+                }
+                return _tubeIdTooltipText;
+            }
+
+            set
+            {
+                _tubeIdTooltipText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand EnterTooltipCommand
+        {
+            get
+            {
+                if (_enterTooltipCommand == null)
+                {
+                    _enterTooltipCommand = new RelayCommand(param =>
+                    {
+                        if (string.IsNullOrWhiteSpace(TubeId))
+                        {
+                            TubeId = TubeIdTooltipText;
+                        }
+                    });
+                }
+                return _enterTooltipCommand;
+            }
+        }
+
+        public string TubeId
+        {
+            get => _tubeId; set
+            {
+                _tubeId = value;
+                OnPropertyChanged();
             }
         }
     }
