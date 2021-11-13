1baff653ae7ce6e0b66cdf04cb7259bd2a3e34a3
thepicture <85333076+thepicture@users.noreply.github.com>
2021-11-13 07:00:35 +0000

    Added sorting and filtration at AdminView

--- ViewModels/AdminViewModel.cs
+++ ViewModels/AdminViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using LaboratoryAppMVVM.Models.Entities;
 using LaboratoryAppMVVM.Stores;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,11 +11,16 @@ namespace LaboratoryAppMVVM.ViewModels
         private readonly ViewModelNavigationStore navigationStore;
         private List<HistoryOfLogin> _userLoginHistoriesList;
         private List<Service> _servicesList;
+        private string _userLoginText = string.Empty;
+        private LaboratoryDatabaseEntities _context;
+        private List<string> _sortTypes;
+        private string _currentSortType;
 
         public AdminViewModel(ViewModelNavigationStore navigationStore, User user)
         {
             this.navigationStore = navigationStore;
             Title = "Страница администратора";
+            User = user;
         }
 
         public List<HistoryOfLogin> UserLoginHistoriesList
@@ -23,7 +29,7 @@ namespace LaboratoryAppMVVM.ViewModels
             {
                 if (_userLoginHistoriesList == null)
                 {
-                    _userLoginHistoriesList = new LaboratoryDatabaseEntities().HistoryOfLogin.ToList();
+                    _userLoginHistoriesList = Context.HistoryOfLogin.ToList();
                 }
                 return _userLoginHistoriesList;
             }
@@ -40,7 +46,7 @@ namespace LaboratoryAppMVVM.ViewModels
             {
                 if (_servicesList == null)
                 {
-                    _servicesList = new LaboratoryDatabaseEntities().Service.ToList();
+                    _servicesList = Context.Service.ToList();
                 }
                 return _servicesList;
             }
@@ -49,6 +55,89 @@ namespace LaboratoryAppMVVM.ViewModels
             {
                 _servicesList = value;
                 OnPropertyChanged();
+            }
+        }
+
+        public string UserLoginText
+        {
+            get => _userLoginText; set
+            {
+                _userLoginText = value;
+                if (!string.IsNullOrWhiteSpace(_userLoginText))
+                {
+                    FilterUserLoginHistory();
+                }
+                OnPropertyChanged();
+            }
+        }
+
+        public LaboratoryDatabaseEntities Context
+        {
+            get
+            {
+                if (_context == null)
+                {
+                    _context = new LaboratoryDatabaseEntities();
+                }
+                return _context;
+            }
+
+            set
+            {
+                _context = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public List<string> SortTypes
+        {
+            get
+            {
+                if (_sortTypes == null)
+                {
+                    _sortTypes = new List<string>
+                    {
+                        "Без сортировки",
+                        "По возрастанию по дате попытки входа",
+                        "По убыванию по дате попытки входа",
+                    };
+                    _currentSortType = _sortTypes.First();
+                }
+                return _sortTypes;
+            }
+
+            set
+            {
+                _sortTypes = value;
+                OnPropertyChanged();
+            }
+        }
+        public string CurrentSortType
+        {
+            get => _currentSortType; set
+            {
+                _currentSortType = value;
+                FilterUserLoginHistory();
+                OnPropertyChanged();
+            }
+        }
+
+        private void FilterUserLoginHistory()
+        {
+            UserLoginHistoriesList = Context
+                .HistoryOfLogin
+                .Where(history => history.User.Login.ToLower().Contains(UserLoginText))
+                .ToList();
+            switch (SortTypes.IndexOf(CurrentSortType))
+            {
+                case 1:
+                    UserLoginHistoriesList = UserLoginHistoriesList.OrderBy(history => history.DateTime).ToList();
+                    break;
+                case 2:
+                    UserLoginHistoriesList = UserLoginHistoriesList.OrderByDescending(history => history.DateTime).ToList();
+                    break;
+                default:
+                    break;
             }
         }
     }
