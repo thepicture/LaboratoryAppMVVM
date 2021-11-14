5fc581d982b95b6c39b87778e598fbc420fd1c47
thepicture <85333076+thepicture@users.noreply.github.com>
2021-11-14 08:21:42 +0000

    Added pseudoscanning a tube id from a scanner

--- ViewModels/CreateOrEditOrderView.cs
+++ ViewModels/CreateOrEditOrderView.cs
@@ -10,6 +10,7 @@ using System.IO;
 using System.Linq;
 using System.Windows;
 using System.Windows.Media.Imaging;
+using WIA;
 
 namespace LaboratoryAppMVVM.ViewModels
 {
@@ -22,6 +23,7 @@ namespace LaboratoryAppMVVM.ViewModels
         private RelayCommand _addServiceToServicesViewCommand;
         private RelayCommand _navigateToLaboratoryAssistantViewModel;
         private RelayCommand _enterTooltipCommand;
+        private RelayCommand _getBarcodeFromScannerCommand;
         private string _tubeIdTooltipText = "Введите код пробирки...";
         private string _tubeId;
         private RenderTargetBitmap _barcodeBitmap;
@@ -99,7 +101,8 @@ namespace LaboratoryAppMVVM.ViewModels
         {
             get
             {
-                if (_navigateToLaboratoryAssistantViewModel == null)
+                if (_navigateToLaboratoryAssistantViewModel
+                    == null)
                 {
                     _navigateToLaboratoryAssistantViewModel =
                         new RelayCommand(param => _navigationStore.CurrentViewModel =
@@ -115,7 +118,9 @@ namespace LaboratoryAppMVVM.ViewModels
             {
                 if (Context.Order.Any())
                 {
-                    _tubeIdTooltipText = Convert.ToString(Context.Order.ToList().Last().Id + 1);
+                    _tubeIdTooltipText = Convert.ToString(
+                        Context.Order.ToList().Last().Id + 1
+                        );
                 }
                 return _tubeIdTooltipText;
             }
@@ -133,55 +138,77 @@ namespace LaboratoryAppMVVM.ViewModels
             {
                 if (_enterTooltipCommand == null)
                 {
-                    _enterTooltipCommand = new RelayCommand(param =>
-                    {
-                        if (string.IsNullOrWhiteSpace(TubeId))
-                        {
-                            TubeId = TubeIdTooltipText;
-                        }
-                        else
-                        {
-                            string tempBarCodePath = AppDomain
-                            .CurrentDomain
-                            .BaseDirectory + "tempBarcode.png";
-                            BarcodeBitmap = new BarcodeImageGenerator($"{TubeId}" +
-                                $"{DateTime.Now:ddMMyyyy}" +
-                                $"{new Random().Next(100000, 999999 + 1)}").Generate(200, 40);
-                            PngBitmapEncoder encoder = new PngBitmapEncoder();
-                            try
-                            {
-                                encoder.Frames.Add(BitmapFrame.Create(BarcodeBitmap));
-                                using (FileStream stream = new FileStream(tempBarCodePath, FileMode.Create))
-                                {
-                                    encoder.Save(stream);
-                                }
-                                string filePath = new CustomPathPdfExporter(new BarcodePdfExporter())
-                                .Save(isShowAfterSave: true);
-                                if (!string.IsNullOrWhiteSpace(filePath))
-                                {
-                                    MessageBoxService.ShowInformation("Документ успешно сохранён по пути " +
-                                        filePath +
-                                        "Путь скопирован в буфер обмена");
-                                    Clipboard.SetText(filePath);
-                                }
-                            }
-                            catch (PdfExportException ex)
-                            {
-                                MessageBoxService.ShowError("Не удалось сохранить штрих-код. " +
-                                    "Пожалуйста, попробуйте сохранить файл ещё раз. " +
-                                    "Ошибка: " + ex.Message);
-                            }
-                            finally
-                            {
-                                if (File.Exists(tempBarCodePath))
-                                {
-                                    File.Delete(tempBarCodePath);
-                                }
-                            }
-                        }
-                    });
+                    _enterTooltipCommand =
+                        new RelayCommand(param => GeneratePdfBarcode());
                 }
                 return _enterTooltipCommand;
+            }
+        }
+
+        private void GeneratePdfBarcode()
+        {
+            if (string.IsNullOrWhiteSpace(TubeId))
+            {
+                TubeId = TubeIdTooltipText;
+            }
+            else
+            {
+                string tempBarCodePath = AppDomain
+                .CurrentDomain
+                .BaseDirectory + "tempBarcode.png";
+                string barcodeText = $"{TubeId}" +
+                                    $"{DateTime.Now:ddMMyyyy}" +
+                                    $"{new Random().Next(100000, 999999 + 1)}";
+                try
+                {
+                    SaveBarcodeToPdfFile(tempBarCodePath, barcodeText);
+                }
+                catch (PdfExportException ex)
+                {
+                    MessageBoxService.ShowError("Не удалось " +
+                        "сохранить штрих-код. " +
+                        "Пожалуйста, попробуйте сохранить " +
+                        "файл ещё раз. " +
+                        "Ошибка: " + ex.Message);
+                }
+                finally
+                {
+                    if (File.Exists(tempBarCodePath))
+                    {
+                        File.Delete(tempBarCodePath);
+                    }
+                }
+            }
+        }
+
+        private void SaveBarcodeToPdfFile(string tempBarCodePath,
+                                          string barcodeText)
+        {
+            BarcodeBitmap = new BarcodeImageGenerator(
+    barcodeText)
+    .Generate(200, 40);
+            PngBitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(BarcodeBitmap));
+            using (FileStream stream = new FileStream(
+                tempBarCodePath,
+                FileMode.Create))
+            {
+                encoder.Save(stream);
+            }
+            string filePath = new CustomPathPdfExporter(
+                pdfExportable: new BarcodePdfExporter()
+                )
+            .Save(isShowAfterSave: true);
+            bool fileWasSaved = !string.IsNullOrWhiteSpace(
+                filePath);
+
+            if (fileWasSaved)
+            {
+                MessageBoxService.ShowInformation("Документ " +
+                    "успешно сохранён по пути " +
+                    filePath +
+                    "Путь скопирован в буфер обмена");
+                Clipboard.SetText(filePath);
             }
         }
 
@@ -201,6 +228,66 @@ namespace LaboratoryAppMVVM.ViewModels
                 _barcodeBitmap = value;
                 OnPropertyChanged();
             }
+        }
+
+        public RelayCommand GetBarcodeFromScannerCommand
+        {
+            get
+            {
+                if (_getBarcodeFromScannerCommand == null)
+                {
+                    _getBarcodeFromScannerCommand =
+                        new RelayCommand(param =>
+                        GetBarcodeForScanner());
+                }
+                return _getBarcodeFromScannerCommand;
+            }
+        }
+
+        private void GetBarcodeForScanner()
+        {
+            DeviceManager deviceManager = new DeviceManager();
+            DeviceInfo scanner = null;
+
+            for (int i = 1;
+                     i < deviceManager.DeviceInfos.Count;
+                     i++)
+            {
+                if (deviceManager.DeviceInfos[i].Type
+                    != WiaDeviceType.ScannerDeviceType)
+                {
+                    continue;
+                }
+
+                scanner = deviceManager.DeviceInfos[i];
+            }
+
+            if (scanner == null)
+            {
+                MessageBoxService.ShowError("Сканеры " +
+                    "не обнаружены " +
+                    "на вашей рабочей системе. " +
+                    "Пожалуйста, проверьте " +
+                    "подключение сканера " +
+                    "к вашему USB-порту");
+                return;
+            }
+
+            Device device = scanner.Connect();
+            Item item = device.Items[1];
+            ImageFile imageFile = (ImageFile)item
+                .Transfer(FormatID.wiaFormatPNG);
+            string barcode = ParseCodeFromImageFile(imageFile);
+            TubeId = barcode.Replace("\r", "");
+
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Method will be extended later.")]
+        private string ParseCodeFromImageFile(ImageFile imageFile)
+        {
+            return $"{new Random().Next(100)}"
+                + $"{DateTime.Now:ddMMyyyy}"
+                + $"{new Random().Next(100000, 999999 + 1)}";
         }
     }
 }
